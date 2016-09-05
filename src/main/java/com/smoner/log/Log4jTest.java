2c55f9e60b23f97f111564d448b90134b71cfb84
fengjqc <fengjqc@fengjqci.ufsoft.com.cn>
2016-09-05 20:59:42 +0800

    update maven-source-plugin to 3.0.1
    Signed-off-by: fengjqc <fengjqc@fengjqci.ufsoft.com.cn>

--- src/main/java/com/smoner/log/Log4jTest.java
+++ src/main/java/com/smoner/log/Log4jTest.java
@@ -3,19 +3,6 @@ package com.smoner.log;
 import org.apache.log4j.Logger;
 
 public class Log4jTest {
-  /**
-   * 方法功能描述：
-   * <p>
-*
-   * <b>参数说明</b>
-   * 
-   * @param args
-   *          <p>
-   * @since 6.36
-   * @author fengjqc
-   * 
-   * @time 2016年6月3日 上午2:00:37
-   */
   public static void main(String[] args) {
     Logger log = Logger.getLogger(Log4jTest.class);
     for (int i = 0; i < 10000; i++) {
